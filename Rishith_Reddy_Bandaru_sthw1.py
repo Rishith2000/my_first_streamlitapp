a30ef8f6bc9c07fdade7f2d42941a3cbd0953187
Rishith2000 <71174752+Rishith2000@users.noreply.github.com>
2022-10-31 21:49:08 -0400

    Add files via upload

---
+++ Rishith_Reddy_Bandaru_sthw1.py
@@ -0,0 +1,96 @@
+import streamlit as st
+import random
+import altair as alt
+import numpy as np
+import pandas as pd
+
+st.header('Homework 1')
+
+st.markdown(
+"**QUESTION 1**: In previous homeworks you created dataframes from random numbers.\n"
+"Create a datframe where the x axis limit is 100 and the y values are random values.\n"
+"Print the dataframe you create and use the following code block to help get you started"
+)
+
+st.code(
+''' 
+x_limit = 
+
+# List of values from 0 to 100 each value being 1 greater than the last
+x_axis = np.arange()
+
+# Create a random array of data that we will use for our y values
+y_data = []
+
+df = pd.DataFrame({'x': x_axis,
+                     'y': y_data})
+st.write(df)''',language='python')
+x_limit=100
+y_data=[random.random() for i in range(0,x_limit)]
+x_axis=np.arange(0,100,1)
+df=pd.DataFrame({'x': x_axis,
+                     'y': y_data },columns=['x','y'])
+st.write(df)
+
+st.markdown(
+"**QUESTION 2**: Using the dataframe you just created, create a basic scatterplot and Print it.\n"
+"Use the following code block to help get you started."
+)
+
+st.code(
+''' 
+scatter = alt.Chart().mark_point().encode()
+
+st.altair_chart(scatter, use_container_width=True)''',language='python')
+
+scatter = alt.Chart(df).mark_point().encode(x='x',y='y')
+
+st.altair_chart(scatter, use_container_width=True)
+st.markdown(
+"**QUESTION 3**: Lets make some edits to the chart by reading the documentation on Altair.\n"
+"https://docs.streamlit.io/library/api-reference/charts/st.altair_chart.  "
+"Make 5 changes to the graph, document the 5 changes you made using st.markdown(), and print the new scatterplot.  \n"
+"To make the bullet points and learn more about st.markdown() refer to the following discussion.\n"
+"https://discuss.streamlit.io/t/how-to-indent-bullet-point-list-items/28594/3"
+)
+
+st.markdown("The five changes I made were.....")
+st.markdown("""
+The 5 changes I made were:
+- Marking as squares
+- Adding tooltip
+- Adding Title
+- Making the chart Interactive
+- Adding colors
+""")
+
+scatter = alt.Chart(df, title='Scatterplot').interactive().mark_square().encode(x='x',y='y',color='y',tooltip=['x','y'])
+
+st.altair_chart(scatter, use_container_width=True)
+
+st.markdown(
+"**QUESTION 4**: Explore on your own!  Go visit https://altair-viz.github.io/gallery/index.html.\n "
+"Pick a random visual, make two visual changes to it, document those changes, and plot the visual.  \n"
+"You may need to pip install in our terminal for example pip install vega_datasets "
+)
+
+st.markdown("""
+The 2 changes I made were:
+- Adding Colour
+- Adding Title
+- Adding Tooltip
+"""
+)
+
+import altair as alt
+
+source = pd.DataFrame({
+    'a': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
+    'b': [28, 55, 43, 91, 81, 53, 19, 87, 52]
+})
+
+k=alt.Chart(source,title='Basic bar chart').interactive().mark_bar().encode(
+    x='a',
+    y='b', color='a',tooltip=['a','b']
+)
+st.altair_chart(k)
